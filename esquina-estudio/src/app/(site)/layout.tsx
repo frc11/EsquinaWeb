707c61dbcf22af8c2dea8def299e3389809685ba
Valentino Olmedo <valenolme@gmail.com>
2026-05-22 14:51:22 -0300

    commit transiciones actualizadas, probando animacion de punto

--- esquina-estudio/src/app/(site)/layout.tsx
+++ esquina-estudio/src/app/(site)/layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import FullLayoutTransitionShell from "@/components/layout/FullLayoutTransitionShell";
 import PageTransitionShell from "@/components/layout/PageTransitionShell";
 import RouteTransitionProvider from "@/components/layout/RouteTransitionProvider";
 import SmoothScrollProvider from "@/components/providers/SmoothScrollProvider";
@@ -13,14 +12,11 @@ export default function SiteLayout({
   return (
     <SmoothScrollProvider>
       <RouteTransitionProvider>
-        <FullLayoutTransitionShell>
-  <Navbar />
-  <main className="pt-[var(--header-height)]">
-    <PageTransitionShell>{children}</PageTransitionShell>
-  </main>
-</FullLayoutTransitionShell>
-
-<Footer />
+        <Navbar />
+        <PageTransitionShell>
+          <main className="pt-[var(--header-height)]">{children}</main>
+          <Footer />
+        </PageTransitionShell>
       </RouteTransitionProvider>
     </SmoothScrollProvider>
   );
